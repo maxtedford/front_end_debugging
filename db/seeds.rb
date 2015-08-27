54bc6d549f072b02988238ae544a88a6d852df43
Brittany Storoz <brittanystoroz@gmail.com>
2015-08-27 02:16:35 -0400

    Part 1 setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    tasks = Task.create([
+      { name: 'Lorem ipsum dolor set amet', due_date: "27/08/2015" },
+      { name: 'Etiam non arcu at nisl aliquet ultricies', due_date: "27/08/2015" },
+      { name: 'Nullam lobortis neque eu eros sollicitudin dignissim.', due_date: "28/08/2015" },
+      { name: 'Vestibulum libero est, eleifend ut sem vel', due_date: "28/08/2015" },
+      { name: 'Nam rhoncus nibh quis sollicitudin accumsan.', due_date: "29/08/2015" },
+
+    ]);
